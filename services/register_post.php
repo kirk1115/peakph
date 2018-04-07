12c823590e3a21e39adf25877c26c5abe5706174
kirk1115 <kirk.padilla.15@gmail.com>
2018-04-07 16:26:12 +0800

    Changed header

--- services/register_post.php
+++ services/register_post.php
@@ -12,8 +12,11 @@ $details = $_POST['details'];
 $totalExpenses = $_POST['totalExpenses'];
 
 $imgContent = base64_encode(file_get_contents($compressedImage));
-$result = mysqli_query($dbconnect,
-"INSERT INTO `posts`(`UserId`, `Title`, `Location`, `Details`, `TotalExpenses`, `CoverPhoto`, `Status`) ".
-"VALUES ('$userId','$title','$location','$details','$totalExpenses', '$imgContent','1')");
-echo mysqli_insert_id($dbconnect);
+// $result = mysqli_query($dbconnect,
+// "INSERT INTO `posts`(`UserId`, `Title`, `Location`, `Details`, `TotalExpenses`, `CoverPhoto`, `Status`) ".
+// "VALUES ('$userId','$title','$location','$details','$totalExpenses', '$imgContent','1')");
+// echo mysqli_insert_id($dbconnect);
+echo $userId;
+echo $title;
+
 ?>
